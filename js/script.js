75d6ff7acc6d3f4c06db925c451d95cc9c76d4cb
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-01-10 02:55:31 +0530

    first update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+/*const HEADSIZE_2 = document.querySelector(".head-size-2");
+const HEADSIZE_3 = document.querySelector(".head-size-3");
+
+
+HEADSIZE_3.classList.remove(".hide");
+HEADSIZE_2.classList.add('.hide');
+
+
+function reveal(e) {
+	e.preventDefault();
+	HEADSIZE_2.classList.toggle(".hide");
+	HEADSIZE_3.classList.toggle(".hide");
+	
+}
+
+
+
+
+HEADSIZE_2.addEventListener('hover' , reveal , false);
+HEADSIZE_3.addEventListener('hover', function(){console.log("Hovered")}, false);*/
